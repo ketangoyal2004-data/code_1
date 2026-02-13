3a681ed45c38383f36cd36e90923cedf49e58709
ketangoyal2004-data <ketangoyal858@gmail.com>
2026-02-13 08:29:00 +0530

    This is a new files added

---
+++ demo3.py
@@ -0,0 +1,12 @@
+class name:
+    def name1(self,name):
+        self.name = name
+        print("My name is :",name)
+
+    def name2(self,name):
+        self.name = name
+        print("my name is :",name)
+
+obj = name()
+obj.name1("John")
+obj.name2("Doe")    
---
+++ demo4.py
@@ -0,0 +1,7 @@
+class college:
+    def college1(self,name):
+        self.name = name
+        print(name)
+
+obj = college()
+obj.college1("ABC College")
