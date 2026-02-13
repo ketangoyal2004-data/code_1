1e7856c329b06e54e644f11f21d516e81c146977
ketangoyal2004-data <ketangoyal858@gmail.com>
2026-02-13 08:35:07 +0530

    This is a new files added

--- demo4.py
+++ demo4.py
@@ -3,5 +3,16 @@ class college:
         self.name = name
         print(name)
 
+    def college2(self,name):
+        self.name = name
+        print(name)
+
+    def college3(self,name):
+        self.name = name
+        print(name)
+
+
 obj = college()
 obj.college1("ABC College")
+obj.college2("XYZ College")
+obj.college3("PQR College") 
